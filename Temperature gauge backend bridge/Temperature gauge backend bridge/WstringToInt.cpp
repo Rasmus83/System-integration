dff425a75740567ea01318770f882db49d0389ed
Rasmus83 <rada041028@gmail.com>
2025-09-25 12:20:54 +0200

    Delete Temperature gauge backend bridge directory

--- Temperature gauge backend bridge/Temperature gauge backend bridge/WstringToInt.cpp
+++
@@ -1,9 +0,0 @@
-int atoi(const wchar_t* _String)
-{
-	int result = 0;
-	while (*_String)
-	{
-		result = 10 * result + *_String++ - '0';
-	}
-	return result;
-}
--- Temperature gauge backend bridge/Temperature gauge backend bridge/WstringToInt.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-int atoi(const wchar_t* _String);
